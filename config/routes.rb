697b59b88c5853fdd5ecec382a648389abf818cb
Yasushi Arai <arai.quality@gmail.com>
2020-05-10 21:11:36 +0000

    　Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get  'signup', to: 'users#new'
+  
+  #login function
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
+
   resources :users
 end
