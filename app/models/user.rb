b7121ce68f9d0b02fd3cf853729e5e31fc37783f
Yasushi Arai <arai.quality@gmail.com>
2020-05-16 16:33:50 +0000

    Define remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # remember_tokenという仮想の属性を作成
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,7 +11,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
-  # 渡された文字列のハッシュ値を返します。
+  # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = 
       if ActiveModel::SecurePassword.min_cost
@@ -20,8 +22,14 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # ランダムなトークンを返します。
+  # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+  
+  # 永続的セッションのためハッシュ化したトークンをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
